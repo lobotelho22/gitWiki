3a392989a2d44e3539b78c2053cfbe082e73ad2c
lobotelho22 <eduardo.ocara@gmail.com>
2023-12-30 15:06:51 -0300

    dev: criado estilo global (style.js)

--- src/styles.js
+++ src/styles.js
@@ -1,12 +1,155 @@
 import { createGlobalStyle } from "styled-components";
 
-export default createGlobalStyle `
-    * {
-        margin: 0;
-        padding: 0;
-    }
+export default createGlobalStyle`
+@font-face {
+    font-family: Inter;
+    font-style: normal;
+    font-weight: 400;
+    src: local("Inter"),
+      url(https://fonts.cdnfonts.com/s/19795/Inter[slnt,wght].woff) format("woff");
+  }
+  @font-face {
+    font-family: Inter;
+    font-style: normal;
+    font-weight: 400;
+    src: local("Inter"),
+      url(https://fonts.cdnfonts.com/s/19795/Inter-Regular.woff) format("woff");
+  }
+  @font-face {
+    font-family: Inter;
+    font-style: italic;
+    font-weight: 400;
+    src: local("Inter"),
+      url(https://fonts.cdnfonts.com/s/19795/Inter-Italic.woff) format("woff");
+  }
+  @font-face {
+    font-family: Inter;
+    font-style: normal;
+    font-weight: 100;
+    src: local("Inter"),
+      url(https://fonts.cdnfonts.com/s/19795/Inter-Thin-BETA.woff) format("woff");
+  }
+  @font-face {
+    font-family: Inter;
+    font-style: italic;
+    font-weight: 100;
+    src: local("Inter"),
+      url(https://fonts.cdnfonts.com/s/19795/Inter-ThinItalic-BETA.woff)
+        format("woff");
+  }
+  @font-face {
+    font-family: Inter;
+    font-style: normal;
+    font-weight: 200;
+    src: local("Inter"),
+      url(https://fonts.cdnfonts.com/s/19795/Inter-ExtraLight-BETA.woff)
+        format("woff");
+  }
+  @font-face {
+    font-family: Inter;
+    font-style: italic;
+    font-weight: 200;
+    src: local("Inter"),
+      url(https://fonts.cdnfonts.com/s/19795/Inter-ExtraLightItalic-BETA.woff)
+        format("woff");
+  }
+  @font-face {
+    font-family: Inter;
+    font-style: normal;
+    font-weight: 300;
+    src: local("Inter"),
+      url(https://fonts.cdnfonts.com/s/19795/Inter-Light-BETA.woff) format("woff");
+  }
+  @font-face {
+    font-family: Inter;
+    font-style: italic;
+    font-weight: 300;
+    src: local("Inter"),
+      url(https://fonts.cdnfonts.com/s/19795/Inter-LightItalic-BETA.woff)
+        format("woff");
+  }
+  @font-face {
+    font-family: Inter;
+    font-style: normal;
+    font-weight: 500;
+    src: local("Inter"),
+      url(https://fonts.cdnfonts.com/s/19795/Inter-Medium.woff) format("woff");
+  }
+  @font-face {
+    font-family: Inter;
+    font-style: italic;
+    font-weight: 500;
+    src: local("Inter"),
+      url(https://fonts.cdnfonts.com/s/19795/Inter-MediumItalic.woff)
+        format("woff");
+  }
+  @font-face {
+    font-family: Inter;
+    font-style: normal;
+    font-weight: 600;
+    src: local("Inter"),
+      url(https://fonts.cdnfonts.com/s/19795/Inter-SemiBold.woff) format("woff");
+  }
+  @font-face {
+    font-family: Inter;
+    font-style: italic;
+    font-weight: 600;
+    src: local("Inter"),
+      url(https://fonts.cdnfonts.com/s/19795/Inter-SemiBoldItalic.woff)
+        format("woff");
+  }
+  @font-face {
+    font-family: Inter;
+    font-style: normal;
+    font-weight: 700;
+    src: local("Inter"),
+      url(https://fonts.cdnfonts.com/s/19795/Inter-Bold.woff) format("woff");
+  }
+  @font-face {
+    font-family: Inter;
+    font-style: italic;
+    font-weight: 700;
+    src: local("Inter"),
+      url(https://fonts.cdnfonts.com/s/19795/Inter-BoldItalic.woff) format("woff");
+  }
+  @font-face {
+    font-family: Inter;
+    font-style: normal;
+    font-weight: 800;
+    src: local("Inter"),
+      url(https://fonts.cdnfonts.com/s/19795/Inter-ExtraBold.woff) format("woff");
+  }
+  @font-face {
+    font-family: Inter;
+    font-style: italic;
+    font-weight: 800;
+    src: local("Inter"),
+      url(https://fonts.cdnfonts.com/s/19795/Inter-ExtraBoldItalic.woff)
+        format("woff");
+  }
+  @font-face {
+    font-family: Inter;
+    font-style: normal;
+    font-weight: 900;
+    src: local("Inter"),
+      url(https://fonts.cdnfonts.com/s/19795/Inter-Black.woff) format("woff");
+  }
+  @font-face {
+    font-family: Inter;
+    font-style: italic;
+    font-weight: 900;
+    src: local("Inter"),
+      url(https://fonts.cdnfonts.com/s/19795/Inter-BlackItalic.woff)
+        format("woff");
+  }
+  
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
-    body {
-        background-color: #22272e;
-    }
+  body {
+    background-color: #22272e;
+  }
 `
